29f0f02e2e965624c41c53f109e32505ac0c321c
AlejaH1213 <62815863+AlejaH1213@users.noreply.github.com>
2023-05-07 18:05:07 -0700

    Add files via upload
    new version

--- treasure.js
+++ treasure.js
@@ -1,47 +1,27 @@
-
 var treasureNumber = Math.floor(Math.random() *25)
-
 var bombNumber = Math.floor(Math.random() *25)
-
 var treasureNumber2 = Math.floor(Math.random()*25)
-
 var bombNumber2 = Math.floor(Math.random()*25)
-
 var counter = 5
+
     
 const treasure = (location) => {
-    if (counter === 1){
-        alert('one last try!')
-    }
     if(counter === 0){
         alert('sorry, you are a loser')
         return
     }
     if (treasureNumber === bombNumber){
     location.reload()
-   }else if (treasureNumber === location){
-        document.getElementById(location).innerHTML = "💰"
-    }else if (treasureNumber2 === location){
-        document.getElementById(location).innerHTML = "💰"
     }
-    else if(bombNumber ===location){
-        document.getElementById(location).innerHTML = "💣"
-    }else if(bombNumber2 ===location){
-        document.getElementById(location).innerHTML = "💣"
+    else if (treasureNumber === location || treasureNumber2 === location){
+        document.getElementById("table").innerHTML = "<img src=pirate-dance.gif>"
+        return alert ("You won!");
+    }
+    else if(bombNumber ===location || bombNumber2 ===location){
+        document.getElementById("table").innerHTML = "<img src=bomb.gif>"
+        return alert ('You are dead, try again');
     }else {
-        document.getElementById(location).innerHTML = "🌳"
+        document.getElementById(location).innerHTML = "🌴"
         document.getElementById ("counter").innerHTML = counter -= 1
     }
-    
-    if(treasureNumber=== location){
-        alert('congrats you are rich pirate!')
-    }else if (treasureNumber2=== location){
-        alert('congrats you are rich pirate!')
-    }
-    if (bombNumber === location){
-        alert('you are a dead pirate')
-    }else if (bombNumber2 === location){
-        alert('you are a dead pirate')
-    }
 }
-
